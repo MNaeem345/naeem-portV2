3af702f46ddeff5b02650f9d8b48e32c1da413d4
Samin <mohammednaeem345@gmail.com>
2023-03-29 18:22:27 -0400

    third commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,66 @@
-import React from 'react';
-import '../components/Main.css'
+import React, { useState, useEffect } from 'react';
+import '../components/Main.css';
+import Container from '@material-ui/core/Container';
+import Typed from 'react-typed';
+
 
 
 function Main(){
-    return (
-        <div className='about-component'>
-           <p className='about-title'>Hello, My name is</p>
-           <h1>Mohammed Naeem.</h1>
-           <h1>And I love building things!</h1>
+    const [text, setText] = useState("")
+    const [fullText, setFullText] = useState(
+        "Hello my name is"
+  )
+    const [index, setIndex] = useState(0)
+    useEffect(() => {
+        if (index < fullText.length) {
+        setTimeout(() => {
+            setText(text + fullText[index])
+            setIndex(index + 1)
+        }, 150)
+        }
+    }, [index])
 
-        </div>
+
+
+    const [text2, setText2] = useState("")
+    const [fullText2, setFullText2] = useState(
+        "Mohammed Naeem"
+  )
+    const [index2, setIndex2] = useState(0)
+    useEffect(() => {
+        if (index2 < fullText2.length) {
+        setTimeout(() => {
+            setText2(text2 + fullText2[index2])
+            setIndex2(index2 + 1)
+        }, 200)
+        }
+    }, [index2])
+    
+           
+
+    return (
+        <Container maxWidth='md' className='about-component'>
+            
+            <p className='about-title'>{text}</p>
+            <h1>{text2}</h1>
+            
+            <h2><Typed 
+                strings={["I love developing.",
+                        "I love building.",
+                        "I love solving problems.",
+                        "Welcome to my portfolio!"]}
+                typeSpeed = {100}
+                backSpeed = {50}
+                loop
+                
+                />
+            </h2>
+            <p className='about-me'>I am a Front-End developer looking to create responsive and eye catching applications that users can find 
+                apealing. 
+            </p>
+      
+
+        </Container>
     )
 }
 
