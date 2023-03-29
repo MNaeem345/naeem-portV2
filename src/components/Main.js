a660ec3f7a231056ce84d34c4c40b76f3e5b9ba8
Samin <mohammednaeem345@gmail.com>
2023-03-29 01:34:10 -0400

    second commit

---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../components/Main.css'
+
+
+function Main(){
+    return (
+        <div className='about-component'>
+           <p className='about-title'>Hello, My name is</p>
+           <h1>Mohammed Naeem.</h1>
+           <h1>And I love building things!</h1>
+
+        </div>
+    )
+}
+
+export default Main;
